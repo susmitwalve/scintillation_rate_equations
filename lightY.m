d211b86ea9393734e100f572f15edac261913ce7
Susmit Walve <68910166+susmitwalve@users.noreply.github.com>
2022-02-08 23:34:32 +0530

    Delete lightY.m

--- lightY.m
+++
@@ -1,9 +0,0 @@
-function LY1=lightY(dxf,len,dect,ddxt,decaymat,ddxf,LY1)
-for k=1:length(ddxf)
-    for jt=1:len
-        dect(jt)=interp1(ddxt,decaymat(:,jt),ddxf(k),'spline');        
-    end
-    LY1=LY1+((dxf)*dect/(length(ddxf)));
-    disp(k)
-end
-end

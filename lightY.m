7fdc4d35a75279155324001538fc39c4043e588e
Susmit Walve <68910166+susmitwalve@users.noreply.github.com>
2022-02-08 23:24:51 +0530

    Update lightY.m

--- lightY.m
+++ lightY.m
@@ -1,8 +1,7 @@
 function LY1=lightY(dxf,len,dect,ddxt,decaymat,ddxf,LY1)
-for k=1:length(ddxf)%at a particular value of dE/dx
-    for jt=1:len%at a particular time
-        dect(jt)=interp1(ddxt,decaymat(:,jt),ddxf(k),'spline');
-        
+for k=1:length(ddxf)
+    for jt=1:len
+        dect(jt)=interp1(ddxt,decaymat(:,jt),ddxf(k),'spline');        
     end
     LY1=LY1+((dxf)*dect/(length(ddxf)));
     disp(k)
